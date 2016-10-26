333151a85d2d3fdc80d775857c9b1da7ef1bbb07
Attila Laszlo <attila.laszlo@slamby.com>
2016-10-26 15:16:59 +0200

    wiating for the stream to close

--- src/app.js
+++ src/app.js
@@ -28,38 +28,40 @@ app.post('/', function (req, res) {
             if (release.assets[i].name != "docker-compose.yml") continue;
             var composeFileUrl = release.assets[i].browser_download_url;
 
-            request(composeFileUrl).pipe(fs.createWriteStream(composeFilePath));
-            
-            shell.exec(`docker exec ${apiContainerName} printenv ${apiSecretSetting}`, {silent:true}, function(code, stdout, stderr) {
-                if (code === 0) {
-                    var secret = `Slamby ${stdout.trim()}`;
-                    var requestSecret = req.get("Authorization");
-                    if (requestSecret == secret) {
-                        shell.exec(`docker-compose -f ${composeFilePath} up -d --remove-orphans`, function(code, stdout, stderr) {
-                            if (code === 0) {
-                                responseObj.Log = stderr;
+            var composeStream = fs.createWriteStream(composeFilePath);
+            request(composeFileUrl).pipe(composeStream);
 
-                                //delete the old images from the vm
-                                shell.exec('docker rmi $(docker images -q)');
+            composeStream.on('close', function() {
+                shell.exec(`docker exec ${apiContainerName} printenv ${apiSecretSetting}`, {silent:true}, function(code, stdout, stderr) {
+                    if (code === 0) {
+                        var secret = `Slamby ${stdout.trim()}`;
+                        var requestSecret = req.get("Authorization");
+                        if (requestSecret == secret) {
+                            shell.exec(`docker-compose -f ${composeFilePath} up -d --remove-orphans`, function(code, stdout, stderr) {
+                                if (code === 0) {
+                                    responseObj.Log = stderr;
 
-                                res.status(200).send(responseObj);
-                            } else {
-                                console.error(stderr);
-                                errorObj.Errors.push(stderr);
-                                res.status(500).json(errorObj);
-                            }
-                        });
+                                    //delete the old images from the vm
+                                    shell.exec('docker rmi $(docker images -q)');
+
+                                    res.status(200).send(responseObj);
+                                } else {
+                                    console.error(stderr);
+                                    errorObj.Errors.push(stderr);
+                                    res.status(500).json(errorObj);
+                                }
+                            });
+                        } else {
+                            console.error("Authentication failed!");
+                            res.status(401).send();
+                        }
                     } else {
-                        console.error("Authentication failed!");
-                        res.status(401).send();
+                        console.error(stderr);
+                        errorObj.Errors.push("The problem can be that the Slamby API container is not running!");
+                        res.status(500).json(errorObj);
                     }
-                } else {
-                    console.error(stderr);
-                    errorObj.Errors.push("The problem can be that the Slamby API container is not running!");
-                    res.status(500).json(errorObj);
-                }
+                });
             });
-            
         }
     });
 });
