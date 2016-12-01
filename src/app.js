5b29a61c3ee858c04ebd3dedebcfcdbdf7343b1e
Attila Laszlo <attila.laszlo@slamby.com>
2016-12-01 13:28:56 +0100

    add existing env vars

--- src/app.js
+++ src/app.js
@@ -4,63 +4,101 @@ var github = require('octonode');
 var request = require('request');
 var fs = require('fs');
 var shell = require('shelljs');
+var YAML = require('yamljs');
 
 const composeFilePath = 'tmp/docker-compose.yml';
 const apiContainerName = 'slamby_api';
-const apiSecretSetting = 'SlambyApi__ApiSecret';
+const apiEnvVarPrefix = 'SlambyApi';
+var inProgress = false;
 
 process.env['COMPOSE_PROJECT_NAME'] = 'slamby';
 
 app.get('/', function (req, res) {
-    res.status(200).send("OK");
+    res.status(200).send("OK");   
 });
 
 app.post('/', function (req, res) {
-    var version = req.params['version']; 
-    var client = github.client();
-    
     var errorObj = { Errors: ["There was an error during the update process!"] };
-    var responseObj = { Log: ""};
+    if (inProgress) {
+        errorObj.Errors.push("Update in progress!");
+        res.status(500).json(errorObj);
+        return;
+    }
+    
+    try {
+        inProgress = true;
+        console.log('Party is starting here!');
+        var client = github.client();
+    
+        var responseObj = { Log: ""};
 
-    var ghrelease = client.release('slamby/slamby-api', 'latest');
-    ghrelease.info(function(err, release, headers) {
-        if (err != null){
-            console.error(err);
-            res.status(500).json(errorObj);
-        }
-        for (var i = 0, len = release.assets.length; i < len; i++) {
-            if (release.assets[i].name != "docker-compose-wu.yml") continue;
-            var composeFileUrl = release.assets[i].browser_download_url;
+        var ghrelease = client.release('slamby/slamby-api', 'latest');
+        ghrelease.info(function(err, release, headers) {
+            if (err != null){
+                console.error(err);
+                res.status(500).json(errorObj);
+            }
+            for (var i = 0, len = release.assets.length; i < len; i++) {
+                if (release.assets[i].name != "docker-compose-wu.yml") continue;
+                var composeFileUrl = release.assets[i].browser_download_url;
+                
+                console.log(`Compose file found, the URL is: ${composeFileUrl}`);
 
-            var composeStream = fs.createWriteStream(composeFilePath);
-            request(composeFileUrl).pipe(composeStream);
+                var composeStream = fs.createWriteStream(composeFilePath);
+                request(composeFileUrl).pipe(composeStream);
 
-            composeStream.on('close', function() {
-                shell.exec(`docker ps`, {silent:true}, function(code, stdout, stderr) {
-                    if (code === 0) {
-                        shell.exec(`docker-compose -f ${composeFilePath} up -d`, function(code, stdout, stderr) {
-                            if (code === 0) {
-                                responseObj.Log = stderr;
+                composeStream.on('close', function() {
+                    var nativeObject = YAML.load(composeFilePath);
 
-                                //delete the old images from the vm
-                                shell.exec('docker rmi $(docker images -q)');
+                    console.log(`Compose file loaded..`);
 
-                                res.status(200).send(responseObj);
-                            } else {
-                                console.error(stderr);
-                                errorObj.Errors.push(stderr);
-                                res.status(500).json(errorObj);
+                    shell.exec(`docker inspect ${apiContainerName}`, {silent:true}, function(code, stdout, stderr) {
+                        if (code === 0) {
+                            var inspectObj = JSON.parse(stdout);
+                            if (inspectObj[0].Config.Env.length > 0){
+                                inspectObj[0].Config.Env.forEach(function(envVar) {
+                                    if (envVar.startsWith(apiEnvVarPrefix)){
+                                        var splitted = envVar.split("=");
+                                        nativeObject.services.slambyapi["environment"][splitted[0]] = splitted[1];
+                                        console.log(`Environment variable was added to the compose file: ${envVar}`);
+                                    }
+                                }, this);
                             }
-                        });
-                    } else {
-                        console.error(stderr);
-                        errorObj.Errors.push("The problem can be that the Slamby API container is not running!");
-                        res.status(500).json(errorObj);
-                    }
+                            fs.writeFileSync(composeFilePath, YAML.stringify(nativeObject, 4));
+                            shell.exec(`docker-compose -f ${composeFilePath} up -d`, function(code, stdout, stderr) {
+                                console.log(`Composing finished`);
+                                if (code === 0) {
+                                    responseObj.Log = stderr;
+
+                                    console.log(`Delete old images...`);
+                                    //delete the old images from the vm
+                                    shell.exec('docker rmi $(docker images -q)');
+                                    
+                                    inProgress = false;
+                                    res.status(200).send(responseObj);
+                                } else {
+                                    inProgress = false;
+                                    console.error(stderr);
+                                    errorObj.Errors.push(stderr);
+                                    res.status(500).json(errorObj);
+                                }
+                            });
+                        } else {
+                            inProgress = false;
+                            console.error(stderr);
+                            errorObj.Errors.push("The problem can be that the Slamby API container is not running!");
+                            res.status(500).json(errorObj);
+                        }
+                    });
                 });
-            });
-        }
-    });
+            }
+        });
+    }
+    catch (err){
+        res.status(500).json(errorObj);
+        inProgress = false;
+    }
+
 });
 
 app.listen(7000, function () {
